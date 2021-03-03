6d374ba1559512e97fc808846a22e04757126a2d
Groomon <37667812+Groomon@users.noreply.github.com>
2021-03-03 21:56:12 +0100

    included JsonConfiguration

---
+++ src/main/java/com/github/groomon/gslib/configuration/json/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes related to creating a Configuration based on a JSON formatted text file.
+ *
+ * Copyright (C) dumptruckman 2013
+ * https://github.com/dumptruckman/JsonConfiguration
+ */
+package com.github.groomon.gslib.configuration.json;
